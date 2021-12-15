b78b1d23fbe44e066c6eb6b8736ad4373a1c0372
Evangelos Lazarakis <lazarakis@synelixis.com>
2021-12-15 14:19:47 +0200

    Imported the requests library and execute a simple get cmd

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from Plugins.plugin import Plugin
+import requests
+
+ip_address= "192.168.1.235"
 
 class FirstPlugin(Plugin):
 
     def run(self):
         if self.is_activated:
-            print("The First Plugin is being run")
+            print("The First Plugin is being run..")
+            x = requests.get("https://" + ip_address)
+            print(x.text)
         else:
             print("The First Plugin can not run because it is not activated")
 
