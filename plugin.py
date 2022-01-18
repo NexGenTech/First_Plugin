99bcb6f4eeaa412c8f3643832a8b3540a8073174
Evangelos Lazarakis <lazarakis@synelixis.com>
2022-01-18 11:48:20 +0200

    Updated the plugin class in order to add more fields

--- plugin.py
+++ plugin.py
@@ -1,26 +1,32 @@
-class Plugin(object):
-	"""
-	The most simple interface to be inherited when creating a plugin.
-	"""
+class Plugin:
+    """
+    The most simple interface to be inherited when creating a plugin.
+    """
 
-	def __init__(self):
-		self.is_activated = False
+    def __init__(self, id: str, active: bool, name: str, description: str, url: str, protocols: list, ports: list) -> None:
+        self.id = id
+        self.active = active 
+        self.name = name
+        self.description = description
+        self.url = url
+        self.protocols = protocols
+        self.ports = ports
 
-	def activate(self):
-		"""
-		Called at plugin activation.
-		"""
-		self.is_activated = True
-		print("The " + self.__class__.__name__ + " has been activated")
+    def activate(self):
+        """
+        Called at plugin activation.
+        """
+        self.is_activated = True
+        print("The " + self.__class__.__name__ + " has been activated")
 
-	def deactivate(self):
-		"""
-		Called when the plugin is disabled.
-		"""
-		self.is_activated = False
-		print("The " + self.__class__.__name__ + " has been deactivated")
+    def deactivate(self):
+        """
+        Called when the plugin is disabled.
+        """
+        self.is_activated = False
+        print("The " + self.__class__.__name__ + " has been deactivated")
 
-	def run(self):
-		"""
-		This function executes the plugin if it is activated
-		"""
+    def run(self):
+        """
+        This function executes the plugin if it is activated
+        """
